c02cbb9550b1e2fcdb5aba001c445eed199e0e33
thelovac <thelovac666@gmail.com>
2014-04-29 15:56:41 -0400

    Iterator v0.2

--- List/Iterator.hpp
+++ List/Iterator.hpp
@@ -7,24 +7,30 @@ Iterator<TYPE>::Iterator(void)
 template <class TYPE>
 void Iterator<TYPE>::Next(void)
 {
+	this->current = current->Next;
+
 }
 
 template <class TYPE>
 void Iterator<TYPE>::Previous(void)
 {
+	this->current = current->Previous;
 }
 
 template <class TYPE>
 TYPE* Iterator<TYPE>::GetCurrentElement(void) const
 {
+	return current->Element;
 }
 
 template <class TYPE>
 Cell<TYPE>* Iterator<TYPE>::GetCurrent(void) const
 {
+	return current;
 }
 
 template <class TYPE>
 void Iterator<TYPE>::SetCurrent(Cell<TYPE>* _cell)
 {
+	this->current = _cell;
 }
